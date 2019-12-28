c880ca9e2408cf13f48aa0957efed566d9bff179
David Latief Budiman <david.budiman91@gmail.com>
2019-12-28 09:22:07 +0700

    Cleaned up App.js

--- src/App.js
+++ src/App.js
@@ -15,93 +15,6 @@ import Hidden from "@material-ui/core/Hidden";
 import Drawer from "@material-ui/core/Drawer";
 import ListItemLink from "./list-item-link/ListItemLink";
 
-export default function App(props) {
-  const { container } = props;
-  const classes = useStyles();
-  const theme = useTheme();
-  const [mobileOpen, setMobileOpen] = React.useState(false);
-
-  const handleDrawerToggle = () => {
-    setMobileOpen(!mobileOpen);
-  };
-
-  const links = [
-    {
-      key: "uncle_bob",
-      label: "Robert C. Martin",
-      route: "/uncle-bob"
-    }
-  ];
-
-  const drawer = (
-    <div>
-      <div className={classes.toolbar} />
-      <Divider />
-      <List>
-        {links.map(item => (
-          <ListItemLink key={item.key} to={item.route} primary={item.label} />
-        ))}
-      </List>
-    </div>
-  );
-
-  return (
-    <BrowserRouter>
-      <div className={classes.root}>
-        <CssBaseline />
-        <AppBar position="fixed" className={classes.appBar}>
-          <Toolbar>
-            <IconButton
-              color="inherit"
-              aria-label="open drawer"
-              edge="start"
-              onClick={handleDrawerToggle}
-              className={classes.menuButton}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" noWrap>
-              Responsive drawer
-            </Typography>
-          </Toolbar>
-        </AppBar>
-        <nav className={classes.drawer} aria-label="mailbox folders">
-          {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
-          <Hidden smUp implementation="css">
-            <Drawer
-              container={container}
-              variant="temporary"
-              anchor={theme.direction === "rtl" ? "right" : "left"}
-              open={mobileOpen}
-              onClose={handleDrawerToggle}
-              classes={{
-                paper: classes.drawerPaper
-              }}
-              ModalProps={{
-                keepMounted: true // Better open performance on mobile.
-              }}
-            >
-              {drawer}
-            </Drawer>
-          </Hidden>
-          <Hidden xsDown implementation="css">
-            <Drawer
-              classes={{
-                paper: classes.drawerPaper
-              }}
-              variant="permanent"
-              open
-            >
-              {drawer}
-            </Drawer>
-          </Hidden>
-        </nav>
-        <AppContainer />
-      </div>
-    </BrowserRouter>
-  );
-}
-
 const drawerWidth = 240;
 
 const useStyles = makeStyles(theme => ({
@@ -135,3 +48,114 @@ const useStyles = makeStyles(theme => ({
     padding: theme.spacing(3)
   }
 }));
+
+export default function App(props) {
+  const { container } = props;
+  const classes = useStyles();
+  const theme = useTheme();
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+  const links = [
+    {
+      key: "uncle_bob",
+      label: "Robert C. Martin",
+      route: "/uncle-bob"
+    }
+  ];
+
+  const routerContainer = () => {
+    return (
+      <BrowserRouter>
+        <div className={classes.root}>
+          <CssBaseline />
+          {appBar()}
+          {navigationDrawer()}
+          <AppContainer />
+        </div>
+      </BrowserRouter>
+    );
+  };
+
+  const appBar = () => {
+    return (
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            className={classes.menuButton}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap>
+            Responsive drawer
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    );
+  };
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const navigationDrawer = () => {
+    return (
+      <nav className={classes.drawer} aria-label="mailbox folders">
+        <Hidden smUp>{mobileDrawer()}</Hidden>
+        <Hidden xsDown>{desktopDrawer()}</Hidden>
+      </nav>
+    );
+  };
+
+  const mobileDrawer = () => {
+    return (
+      <Drawer
+        container={container}
+        variant="temporary"
+        anchor={theme.direction === "rtl" ? "right" : "left"}
+        open={mobileOpen}
+        onClose={handleDrawerToggle}
+        classes={{
+          paper: classes.drawerPaper
+        }}
+        ModalProps={{
+          keepMounted: true
+        }}
+      >
+        {drawerItems()}
+      </Drawer>
+    );
+  };
+
+  const desktopDrawer = () => {
+    return (
+      <Drawer
+        classes={{
+          paper: classes.drawerPaper
+        }}
+        variant="permanent"
+        open
+      >
+        {drawerItems()}
+      </Drawer>
+    );
+  };
+
+  const drawerItems = () => {
+    return (
+      <div>
+        <div className={classes.toolbar} />
+        <Divider />
+        <List>
+          {links.map(item => (
+            <ListItemLink key={item.key} to={item.route} primary={item.label} />
+          ))}
+        </List>
+      </div>
+    );
+  };
+
+  return routerContainer();
+}
