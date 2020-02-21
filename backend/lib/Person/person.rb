0f8d9e36285b3e31e7a05d6364c134a63e834262
David Latief Budiman <david.budiman91@gmail.com>
2020-02-21 23:15:20 +0700

    Restructured workspace of opinionaters

---
+++ backend/lib/Person/person.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# Basic model for Person
+class Person
+  attr_accessor :fullName
+  attr_accessor :personalWebsiteUrl
+  attr_accessor :twitterHandle
+  attr_accessor :rssFeedUrl
+end
---
+++ frontend/src/person/Person.js
@@ -0,0 +1,12 @@
+class Person {
+  fullName;
+  personalWebsiteUrl;
+  twitterHandle;
+  rssFeedUrl;
+  constructor(fullName, personalWebsiteUrl, twitterHandle, rssFeedUrl) {
+    this.fullName = fullName;
+    this.personalWebsiteUrl = personalWebsiteUrl;
+    this.twitterHandle = twitterHandle;
+    this.rssFeedUrl = rssFeedUrl;
+  }
+}
