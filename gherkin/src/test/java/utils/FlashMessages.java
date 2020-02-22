8e077b9312bb4f9762dd09592c6b5446cd9f439e
David Latief Budiman <david.budiman91@gmail.com>
2020-02-22 08:58:03 +0700

    FlashMessages.java syntax fix

--- gherkin/src/test/java/utils/FlashMessages.java
+++ gherkin/src/test/java/utils/FlashMessages.java
@@ -5,13 +5,14 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import utils.driver.Driver;
-import static org.junit.Assert.assertTrue;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class FlashMessages {
     @Step("Show a message <message>")
     public void showAWelcomeMessage(String message) {
         WebDriver webDriver = Driver.webDriver;
         WebElement flashNoticeElement = webDriver.findElement(By.xpath(String.format("//div[@id = 'flash_notice' and text() = '%s']", message)));
-        assertTrue(flashNoticeElement.isDisplayed());
+        assertThat(flashNoticeElement.isDisplayed()).isTrue();
     }
 }
